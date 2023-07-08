46feccbcef8f8478078f054a0219752a232b0887
Ksenia Yurko <iurko.kk@phystech.edu>
2023-07-08 05:42:57 +0300

    vite css plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import stylelint from 'vite-plugin-stylelint';
+import vitePluginStyleImport from 'vite-plugin-style-import';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: 
+    [react(),
+    stylelint({
+      files: '**/*.css',
+    }),
+  //   vitePluginStyleImport({
+  //     libs: [
+  //       {
+  //         libraryName: 'antd',
+  //         esModule: true,
+  //         resolveStyle: (name) => {
+  //           return `antd/es/${name}/style/index`;
+  //         },
+  //       },
+  //     ],
+  //   }),
+  ],
   base: "/vite-project/"
-})
+});
