1f2b758a81ecd73a26f6de80922572bd715b1f30
Ksenia Yurko <iurko.kk@phystech.edu>
2023-07-09 16:24:21 +0300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [splitVendorChunkPlugin(), react()],
   base: 'https://kseniayurko.github.io/shri2023-task/',
+  build: {
+		outDir: "docs",
+	},
 })
