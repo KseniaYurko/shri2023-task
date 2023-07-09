f0681ad30fea1aa546aff916e19c851ab8df0aaa
Ksenia Yurko <iurko.kk@phystech.edu>
2023-07-09 16:05:36 +0300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-// import stylelint from 'vite-plugin-stylelint';
-import vitePluginStyleImport from 'vite-plugin-style-import';
-
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: 
-    [react(),
-    stylelint({
-      files: '**/*.css',
-    }),
-  //   vitePluginStyleImport({
-  //     libs: [
-  //       {
-  //         libraryName: 'antd',
-  //         esModule: true,
-  //         resolveStyle: (name) => {
-  //           return `antd/es/${name}/style/index`;
-  //         },
-  //       },
-  //     ],
-  //   }),
-  ],
-  base: "/vite-project/"
-});
+  plugins: [react()],
+  base: 'https://kseniayurko.github.io/shri2023-task/',
+})
